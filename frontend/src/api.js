220018ce08ad8b2de7f539c25ff69e47385e7649
IsaiasRuizT <isaiasruizt@gmail.com>
2026-08-07 15:29:51 -0400

    change hardcode localhost to render

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-	baseURL: 'http://localhost:5000',
+	baseURL: 'https://capstone-community-marketplace-app.onrender.com',
 });
 
 // Request interceptor: add Authorization header if a token exists
